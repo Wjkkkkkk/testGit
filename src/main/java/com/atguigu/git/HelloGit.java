7721ba67b84d0c2159d02dcbc474c19a47ff6bc1
Wjkkkkkkk <51317084+Wjkkkkkk@users.noreply.github.com>
2020-11-03 21:03:25 +0800

    在线更新

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -5,10 +5,11 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
-		System.out.println("更新t");
-		System.out.println("创建分支");
-		System.out.println("master分支");
-		System.out.println("dev分支");
+		System.out.println("赂眉脨脗t");
+		System.out.println("麓麓陆篓路脰脰搂");
+		System.out.println("master路脰脰搂");
+		System.out.println("dev路脰脰搂");
+		System.out.println("dev路脰脰搂123123");
 		
 	}
 
